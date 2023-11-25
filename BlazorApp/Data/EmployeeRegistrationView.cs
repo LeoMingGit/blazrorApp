86671f9855a943c789849f61f2f6ab24bda3ca0a
kk <kk@example.com>
2023-11-25 14:48:47 +0800

    員工列表頁加入mock data

--- BlazorApp/Data/EmployeeRegistrationView.cs
+++ BlazorApp/Data/EmployeeRegistrationView.cs
@@ -7,6 +7,33 @@ namespace BlazorApp.Data
     /// </summary>
     public class EmployeeRegistrationView
     {
+
+        public EmployeeRegistrationView() {
+
+
+            EmployeeSkills =new List<EmployeeSkillView> { };
+
+            MockData();
+        }   
+
+        public  void MockData()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                EmployeeSkills.Add(new EmployeeSkillView
+                {
+                    SelectedSkill = i % 2 == 0,   
+                    SkillID = i,
+                    Skill = $"Skill {i}",
+                    Level = i % 3 + 1,  
+                    LevelName = $"Level {i}",
+                    YearsOfExperience = i % 2 == 0 ? (int?)i : null,   
+                    HourlyWage = 10.5m + i * 2.0m   
+                });
+            }
+
+        }
+
         /// <summary>
         /// 
         /// </summary>
