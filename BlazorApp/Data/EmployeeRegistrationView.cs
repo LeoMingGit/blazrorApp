e72f40d34d968052182c64ccfed125f8678bfee0
kk <kk@example.com>
2023-11-25 13:25:45 +0800

    提交實體類

---
+++ BlazorApp/Data/EmployeeRegistrationView.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EmployeeRegistrationView
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }= string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
+        public string HomePhone { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isActive { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<EmployeeSkillView> EmployeeSkills { get; set; } = new List<EmployeeSkillView>();    
+    }
+}
---
+++ BlazorApp/Data/EmployeeSkillView.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp.Data
+{
+    public class EmployeeSkillView
+    {
+        public bool SelectedSkill { get; set; }
+        public int SkillID { get; set; }
+        public string Skill { get; set; }
+        public int Level { get; set; }
+        public string LevelName { get; set; }
+        public int? YearsOfExperience { get; set; }
+        public decimal HourlyWage { get; set; }
+    }
+
+}
