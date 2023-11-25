0ce0eaecd9172e2d28f428f0535c9f81c50cb93f
kk <kk@example.com>
2023-11-26 00:24:07 +0800

    数据库连接方法

---
+++ Services/Common/DbConfigs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Common
+{
+    public class DbConfigs
+    {
+        public string Conn { get; set; }
+        public int DbType { get; set; }
+        public string ConfigId { get; set; }
+        public bool IsAutoCloseConnection { get; set; }
+        public string DbName { get; set; }
+    }
+
+}
