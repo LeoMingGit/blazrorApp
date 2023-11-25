497c41777d3561306c7299bee52e801649cea941
kk <kk@example.com>
2023-11-26 00:24:44 +0800

    提交数据库实体

---
+++ Services/Models/Skills.cs
@@ -0,0 +1,22 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Models
+{
+
+    [SugarTable("Skills")]
+    public class Skills
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int SiID { get; set; }
+
+        public string Description { get; set; }
+
+        public string RequiresTicket { get; set; }
+    }
+}
