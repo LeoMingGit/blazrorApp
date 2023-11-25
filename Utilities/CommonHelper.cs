28f6a50dc28c1aba581061580cf9c1a015bd6c7b
kk <kk@example.com>
2023-11-25 17:39:09 +0800

    添加工具類

---
+++ Utilities/CommonHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Utilities
+{
+    public class CommonHelper
+    {
+        public static T DeepCopy<T>(T source)
+        {
+            if (source == null)
+            {
+                return default(T);
+            }
+
+            // 使用 JSON 序列化和反序列化進行深度複製
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true
+            };
+
+            var json = JsonSerializer.Serialize(source, options);
+            return JsonSerializer.Deserialize<T>(json, options);
+        }
+
+    }
+}
