3b2d502c51c6c996aa54d31def86977ac4e0f55b
kk <kk@example.com>
2023-11-25 13:21:08 +0800

    員工註冊界面，部分表單綁定了實體類 默認首頁改成了員工註冊界面

--- BlazorApp/Data/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorApp.Data
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
